6555fb2906de71fa020b7baca29553ddafb417cf
J <98541904+DownMan01@users.noreply.github.com>
2025-02-23 13:56:17 +0800

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,23 +1,31 @@
 import type React from "react"
 import "@/styles/globals.css"
-import { Space_Grotesk } from "next/font/google"
+import { Space_Grotesk } from 'next/font/google'
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "NoteDrop | Digital Art Collection",
-  description: "Discover and collect unique digital art pieces from the future",
+  title: 'NoteDrop',
+  description: 'A collection of well-researched airdrop projects in WEB3.',
+  icons: {
+    icon: '/favico.ico', // /public/favicon.ico
+    shortcut: '/favico.ico', // /public/favicon.ico
+    favico: '/favico.png', // /public/apple-touch-icon.png
+    other: {
+      rel: 'favico',
+      url: '/favico.png'
+    }
+  }
 }
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="en">
+   <html lang="en">
       <body className={spaceGrotesk.className}>{children}</body>
     </html>
   )
 }
-
