de314a2415ae2eace797491c63099f4ec39d5cd2
J <98541904+DownMan01@users.noreply.github.com>
2025-02-23 12:26:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'NoteDrop',
-  description: 'A collection of well-researched airdrop project in WEB3.',
+  description: 'A collection of well-researched airdrop projects in WEB3.',
 }
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="notedrop-ico" href="styles/favico.png" />
+        <link rel="notedrop-ico" href="/styles/favico.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>{children}</body>
