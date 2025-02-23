bfb586372040b9a80144c92f4bfb118dce0f4132
J <98541904+DownMan01@users.noreply.github.com>
2025-02-23 12:34:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'NoteDrop',
   description: 'A collection of well-researched airdrop projects in WEB3.',
+  icons: {
+    icon: '/favicon.ico', // /public/favicon.ico
+    shortcut: '/favicon.ico', // /public/favicon.ico
+    favico: '/favico.png', // /public/apple-touch-icon.png
+    other: {
+      rel: 'favico',
+      url: '/favico.png'
+    }
+  }
 }
 
 export default function RootLayout({
@@ -13,11 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="notedrop-ico" href="/styles/favico.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
       <body>{children}</body>
     </html>
   )
